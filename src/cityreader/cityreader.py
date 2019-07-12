45f9f89c2da5ac917832fb6cf889d2c2abe6bfe6
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-12 23:22:29 +0530

    passed test for cityreader stretch goal

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -23,15 +23,16 @@ class City:
 cities = []
 
 def cityreader(cities=[]):
+  # TODO Implement the functionality to read from the 'cities.csv' file
+  # For each city record, create a new City instance and add it to the 
+  # `cities` list
 
   with open('cities.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
       if row[0] != "city":
         cities.append(City(row[0], float(row[3]), float(row[4])))
-        
     return cities
-
 
 cityreader(cities)
 
@@ -68,14 +69,22 @@ for c in cities:
 # Tucson: (32.1558,-110.8777)
 # Salt Lake City: (40.7774,-111.9301)
 
-# TODO Get latitude and longitude values from the user
+latitude1 = float(input("provide 1st latitude point -> "))
+latitude2 = float(input("provide 2nd latitude point -> "))
+longitude1 = float(input("provide 1st longitude point -> "))
+longitude2 = float(input("provide 2nd longitude point -> "))
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
-  within = []
-
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+  within = [
+    city    
+    for city in cities    
+    if 
+      city.lat <= max(lat1, lat2) and 
+      city.lat >= min(lat1, lat2) and 
+      city.lon <= max(lon1, lon2) and 
+      city.lon >= min(lon1, lon2)
+    ]
 
   return within
+
